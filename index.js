46e8d7f639ef293211c85c7013ca763d1402970f
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-10 01:41:00 +0600

    added review API in server

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('nexaForestDB').collection('services')
+        const reviewCollection = client.db('nexaForestDB').collection('reviews')
         //    console.log(serviceCollection)
         app.get('/services', async (req, res) => {
             const query = {}
@@ -35,6 +36,17 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query)
             res.send(service)
+        })
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+        app.get('/reviews', async (req, res) => {
+            const query = {}
+            const cursor = reviewCollection.find(query)
+            const allReviews = await cursor.toArray()
+            res.send(allReviews)
         })
     }
     finally {
