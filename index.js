1e214bf48f02bff0e30723b5728e350e088a7616
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-09 13:44:31 +0600

    added id wise service routing API, attached with client side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -14,11 +14,35 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsvgbta.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('nexaForestDB').collection('services')
+        //    console.log(serviceCollection)
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+    }
+    finally {
 
-app.get( '/' , (req , res) => {
+    }
+}
+run().catch(err => console.error(err))
+
+
+
+app.get('/', (req, res) => {
     res.send("Nexa Forest server is running")
 })
 
-app.listen( port , () => {
+app.listen(port, () => {
     console.log(`Nexa Forest Server running on port: ${port}`)
 })
